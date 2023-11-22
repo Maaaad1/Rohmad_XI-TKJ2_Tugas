55459095ab6b44002bfc5de84a7df6c41f87c19c
Maaaad1 <132032936+Maaaad1@users.noreply.github.com>
2023-11-23 03:19:12 +0700

    Delete PROYEK_AKHIR_DAFTAR_SISWA directory

--- PROYEK_AKHIR_DAFTAR_SISWA/pyqt.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtGui, QtCore, QtWidgets
-
-app = QtWidgets.QApplication([])
-window = QtWidgets.QPushButton("Tekan Peler")
-window.show()
-
-app.exec()
--- PROYEK_AKHIR_DAFTAR_SISWA/tkinter2.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-
-tes = tkinter.Tk()
-tes.title("Login Dulu Mas Brooo")
-tes.geometry("340x440")
-tes.configure(bg="blue")
-
-tes.mainloop()
